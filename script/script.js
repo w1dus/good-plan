0027606bd42266ddfa94febcb19cc039c6b9bd19
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2026-01-22 18:00:43 +0900

    메인페이지 퍼블 진행중

--- script/script.js
+++ script/script.js
@@ -1,7 +1,16 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    slideMenu();
 })
 
+const slideMenu = () => {
+    $('header .menu-btn').click(function(){
+        $('.slide-menu').addClass('show');
+    })
+
+    $('.slide-menu .btn-div .close-btn').click(function(){
+        $('.slide-menu').removeClass('show');
+    })
+}
 
